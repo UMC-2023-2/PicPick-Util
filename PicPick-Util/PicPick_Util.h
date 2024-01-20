79e63a86907f29dcd377f2ef58d6970e2a8d2133
Jaeuk Jeong <jaeuk@jeuke.com>
2024-01-20 11:10:47 +0900

    Initial Commit

---
+++ PicPick-Util/PicPick_Util.h
@@ -0,0 +1,18 @@
+//
+//  PicPick_Util.h
+//  PicPick-Util
+//
+//  Created by Jaeuk on 1/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PicPick_Util.
+FOUNDATION_EXPORT double PicPick_UtilVersionNumber;
+
+//! Project version string for PicPick_Util.
+FOUNDATION_EXPORT const unsigned char PicPick_UtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PicPick_Util/PublicHeader.h>
+
+
