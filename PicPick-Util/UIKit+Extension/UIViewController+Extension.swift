9f577c0d50bdaa20a7d9921be8cb8ab1106bd283
Jaeuk Jeong <jaeuk@jeuke.com>
2024-02-08 21:03:19 +0900

    add remove back button title func

---
+++ PicPick-Util/UIKit+Extension/UIViewController+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+Extension.swift
+//  PicPick-Util
+//
+//  Created by Jaeuk on 2/8/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    public func removeBackBtnTitle() {
+        navigationItem.backButtonTitle = ""
+    }
+}
