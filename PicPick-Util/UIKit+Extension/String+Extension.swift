470d0af3618c01ecad59ac575fde38be777a62c4
Jaeuk Jeong <jaeuk@jeuke.com>
2024-03-05 23:34:38 +0900

    Fix String extension

--- PicPick-Util/UIKit+Extension/String+Extension.swift
+++ PicPick-Util/UIKit+Extension/String+Extension.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 extension String {
-    func isValidPassword() -> Bool {
+    public func isValidPassword() -> Bool {
         let regularExpression = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}"
         let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regularExpression)
 
         return passwordValidation.evaluate(with: self)
     }
 
-    func isValidEmail() -> Bool {
+    public func isValidEmail() -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
 
